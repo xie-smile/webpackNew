889b6d9787e1ea5a04c7614f9e4043ea1aa383cf
xielei <strivexl@163.com>
2021-02-25 17:44:13 +0800

    webpack commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 // 汇总所有js模块
+// import '@babel/polyfill'
 import {sum} from './module1';
 import {sub} from './module2';
 import module3 from './module3';
 import a from '../json/test.json';
 import "../css/index.less";//在入口文件引入样式 不用变量去接受，不用写from
+
 
 console.log(sum(1, 2));
 console.log(sub(3, 2));
@@ -13,14 +15,14 @@ console.log(a, typeof a);
 
 console.log(1);
 // alert(2);
-console.log("1" === 1);
+console.log("1" == 1);
 console.log("1" == 1)
 let xx = 2;
 xx + 1;
 
 
-function testPromoise(param) {
-    return new Promise((resove, reject) => {
+function testPromise(param) {
+    return new Promise((resove) => {
         if (param) {
             resove("xixi");
         } else {
@@ -29,7 +31,7 @@ function testPromoise(param) {
     });
 }
 
-testPromoise("aa").then((data) => {
+testPromise(true).then((data) => {
     setTimeout(() => {
         console.log(data)
     }, 2000)
@@ -39,7 +41,7 @@ testPromoise("aa").then((data) => {
     }, 2000);
 });
 
-testPromoise().then((data) => {
+/*testPromise(false).then((data) => {
     setTimeout(() => {
         console.log(data)
     }, 3000)
@@ -47,5 +49,7 @@ testPromoise().then((data) => {
     setTimeout(() => {
         console.log(res)
     }, 3000)
-});
+});*/
+let promise = new Promise();
+promise();
 //入口文件在哪
